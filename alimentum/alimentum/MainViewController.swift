ee7aa0f2c6a6c21535ee8e688913572d478ee5e5
Nitish Dayal <nitishdayal@outlook.com>
2016-03-30 02:46:46 -0700

    First actual commit maybe? by Nitish Dayal

---
+++ alimentum/alimentum/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  alimentum
+//
+//  Created by Joseph Park on 3/29/16.
+//  Copyright © 2016 Joseph Park. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
